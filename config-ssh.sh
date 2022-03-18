a01a381b423b2d98d5e62b7dac0c655dd2e7e672
ksy <ehsqjfwk99999@gmail.com>
2022-03-18 17:48:16 +0900

    delete g for sed

--- config-ssh.sh
+++ config-ssh.sh
@@ -6,9 +6,9 @@ sudo true # should be run as root
 # sshd config #
 #=============#
 # enable ssh root login
-sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 # allow ssh password autientication
-sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 
 sudo systemctl restart ssh # apply sshd configuration change
 
@@ -16,4 +16,4 @@ sudo systemctl restart ssh # apply sshd configuration change
 # ssh config #
 #============#
 # automatically add host to '~/.ssh/known_hosts' without asking
-sudo sed -i 's/#   StrictHostKeyChecking ask/StrictHostKeyChecking no/g' /etc/ssh/ssh_config
+sudo sed -i 's/#   StrictHostKeyChecking ask/StrictHostKeyChecking no/' /etc/ssh/ssh_config
