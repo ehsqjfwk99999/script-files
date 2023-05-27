2f131230bdb6036c3262183142813c881b08a066
yarnyyarn <ehsqjfwk99999@gmail.com>
2023-05-27 16:41:57 +0900

    Create unmount.sh

---
+++ unmount.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+
+DEVICE_PATH="/dev/nvme0n1"
+
+echo
+
+# unmount device
+echo -n "Unmount device ... "
+umount $DEVICE_PATH
+echo -e "${GREEN}Done${ENDCOLOR}\n"
