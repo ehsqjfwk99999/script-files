2196d409175eb2a7ec704ab760a7347eb4819fd7
yarnyyarn <ehsqjfwk99999@gmail.com>
2023-06-01 17:50:53 +0900

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
+RED="\e[31m"
 GREEN="\e[32m"
 ENDCOLOR="\e[0m"
 
@@ -10,11 +11,11 @@ MOUNT_PATH="/mnt/fio_test"
 echo
 
 # check root
-[ $(whoami) != "root" ] && echo -e "Login as root first ... Exit\n" && exit
+[ $(whoami) != "root" ] && echo -e "${RED}Login as root first ... Exit${ENDCOLOR}\n" && exit
 
 # Check device path
 echo -n "Checking device path ... "
-! [ -b $DEVICE_PATH ] && echo -e "Error: no device path ... Exit\n" && exit
+! [ -b $DEVICE_PATH ] && echo -e "${RED}Error: no device path ... Exit${ENDCOLOR}\n" && exit
 echo -e "${GREEN}Done${ENDCOLOR}"
 
 # check mount path
@@ -25,7 +26,7 @@ echo -e "${GREEN}Done${ENDCOLOR}"
 # mkfs
 echo -n "mkfs ... "
 mkfs.ext4 -F $DEVICE_PATH
-[ $? != 0 ] && echo -e "Failed: mkfs ... Exit\n" && exit
+[ $? != 0 ] && echo -e "${RED}Failed: mkfs ... Exit${ENDCOLOR}\n" && exit
 echo -e "${GREEN}Done${ENDCOLOR}"
 
 # mount
@@ -34,7 +35,7 @@ echo -n "mount ... "
 # mount -o commit=10 "${DEVICE_PATH}" "${MOUNT_PATH}" &> /dev/null
 # mount -o notreelog,commit=10 "${DEVICE_PATH}" "${MOUNT_PATH}" &> /dev/null
 mount $DEVICE_PATH $MOUNT_PATH
-[ $? != 0 ] && echo -e "Failed: mount ... Exit\n" && exit
+[ $? != 0 ] && echo -e "${RED}Failed: mount ... Exit${ENDCOLOR}\n" && exit
 echo -e "${GREEN}Done${ENDCOLOR}"
 
 echo -e "\n${GREEN}Mount Complete!${ENDCOLOR}\n"
