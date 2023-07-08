07f59ec9ea0a2da939e926b010511d070aba0087
yarnyyarn <ehsqjfwk99999@gmail.com>
2023-07-09 01:33:16 +0900

    Create install_go.sh

---
+++ install_go.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+
+echo -e "${GREEN}Install Golang!${ENDCOLOR}"
+read -p "version: " version
+
+[ -z $version ] && echo -e "\nPlease enter version ... Exit" && exit
+
+# ex) https://go.dev/dl/go1.20.5.linux-amd64.tar.gz
+curl -LO https://go.dev/dl/go${version}.linux-amd64.tar.gz
+
+go_binary_release=go${version}.linux-amd64.tar.gz
+
+# remove previous binary and unzip new binary
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf ${go_binary_release}
+
+# clean up
+rm -rf ${go_binary_release}
+
+echo
+echo -e "${GREEN}Go v${version} installed!${ENDCOLOR}"
+echo
+echo '-> Test with "export PATH=$PATH:/usr/local/go/bin"'
+echo
+
+# export GOROOT=/usr/local/go
+# export GOPATH=<go_path>
+# export PATH=$PATH:$GOROOT/bin:$GOPATH
