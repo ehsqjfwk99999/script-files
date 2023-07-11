b84d7cfdb790b4b4be1b8231915de7879c640f4b
yarnyyarn <ehsqjfwk99999@gmail.com>
2023-07-11 16:57:33 +0900

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -9,6 +9,20 @@ DEVICE_PATH="/dev/nvme0n1"
 MOUNT_PATH="/mnt/fio_test"
 
 echo
+echo -e "${GREEN}Mount Device!${ENDCOLOR}"
+
+# select filesystem
+echo "1) Ext4"
+echo "2) F2FS"
+read -p "Select filesystem [Ext4] " fs
+if [ ${fs:=1} -eq 1 ]; then
+    fs=ext4
+elif [ $fs -eq 2 ]; then
+    fs=f2fs
+else
+    fs=ext4
+fi
+echo
 
 # check root
 [ $(whoami) != "root" ] && echo -e "${RED}Login as root first ... Exit${ENDCOLOR}\n" && exit
@@ -25,17 +39,25 @@ echo -e "${GREEN}Done${ENDCOLOR}"
 
 # mkfs
 echo -n "mkfs ... "
-mkfs.ext4 -F $DEVICE_PATH
+if [ $fs == "ext4" ]; then
+    mkfs.ext4 -F $DEVICE_PATH
+elif [ $fs == "f2fs" ]; then
+    mkfs.f2fs -f $DEVICE_PATH
+fi
 [ $? != 0 ] && echo -e "${RED}Failed: mkfs ... Exit${ENDCOLOR}\n" && exit
 echo -e "${GREEN}Done${ENDCOLOR}"
 
 # mount
 echo -n "mount ... "
-# mount "${DEVICE_PATH}" "${MOUNT_PATH}" &> /dev/null
-# mount -o commit=10 "${DEVICE_PATH}" "${MOUNT_PATH}" &> /dev/null
-# mount -o notreelog,commit=10 "${DEVICE_PATH}" "${MOUNT_PATH}" &> /dev/null
 mount $DEVICE_PATH $MOUNT_PATH
 [ $? != 0 ] && echo -e "${RED}Failed: mount ... Exit${ENDCOLOR}\n" && exit
 echo -e "${GREEN}Done${ENDCOLOR}"
 
 echo -e "\n${GREEN}Mount Complete!${ENDCOLOR}\n"
+
+## [Backup] ####################################################################
+
+# BTRFS mount options
+# mount "${DEVICE_PATH}" "${MOUNT_PATH}" &> /dev/null
+# mount -o commit=10 "${DEVICE_PATH}" "${MOUNT_PATH}" &> /dev/null
+# mount -o notreelog,commit=10 "${DEVICE_PATH}" "${MOUNT_PATH}" &> /dev/null
