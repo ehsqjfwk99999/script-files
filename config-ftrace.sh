33bb6c59934201a0e55115731bb2e0babb5f24f0
yarnyyarn <ehsqjfwk99999@gmail.com>
2023-08-08 14:11:03 +0900

    Update config-ftrace.sh

--- config-ftrace.sh
+++ config-ftrace.sh
@@ -6,7 +6,7 @@ clear_ftrace_configurations() {
     echo 0 >/sys/kernel/debug/tracing/tracing_on
     echo nop >/sys/kernel/debug/tracing/current_tracer
     echo 0 >/sys/kernel/debug/tracing/events/enable
-    echo secondary_start_kernel >/sys/kernel/debug/tracing/set_ftrace_filter
+    echo kernel_init >/sys/kernel/debug/tracing/set_ftrace_filter
     sleep 1
 }
 
